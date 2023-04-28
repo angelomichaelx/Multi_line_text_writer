6657ff565fb5ab6407051ca6f99bda7b6415602f
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-04-29 03:39:40 +0800

    while loop method

--- mylife.py
+++ mylife.py
@@ -1,2 +1,6 @@
 #first we need to input the textbox that we will use in this program which is (mylife.txt)
 with open("mylife.txt", "w") as file:
+#The user is prompted for input using a while loop in this method, and each line is written to the file "mylife.txt" with a newline character at the end.
+#If the user does not wish to enter any further lines, an if statement is used to determine whether the loop should end and the file should be closed.
+    while True:
+            line = input("Enter line: ")
