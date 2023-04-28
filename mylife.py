967da6436a1dc2a4350f858decfa61e3d4145d69
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-04-29 03:54:11 +0800

    add extra color to the text

--- mylife.py
+++ mylife.py
@@ -3,12 +3,11 @@ with open("mylife.txt", "w") as file:
 #The user is prompted for input using a while loop in this method, and each line is written to the file "mylife.txt" with a newline character at the end.
 #If the user does not wish to enter any further lines, an if statement is used to determine whether the loop should end and the file should be closed.
     while True:
-            line = input("Enter line: ")
+            line = input("\033[0;34mEnter line: ")
 # write the line to the file, with a newline character at the end
             file.write(line + "\n") 
 #it will ask you if you want to add more lines by answering y = yes and n = no
-            more_lines = input("Are there more lines y/n? ")
+            more_lines = input("\033[1;32mAre there more lines y/n? ")
 # if there are no lines and answered by 'n' n means NO so the program will stop
             if more_lines.lower() == 'n':
-                break
-
+                  break
